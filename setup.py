42bc238f95673185f4c755f2346a9929558d4242
ug1y <yinhao@icode.pku.edu.cn>
2024-05-31 16:52:05 +0800

    项目准备初始化

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ditto',
+    version='0.1.0',
+    author='ug1y',
+    author_email='yinhao@icode.pku.edu.cn',
+    description='A blockDAG simulation framework',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ug1y/ditto',
+    packages=find_packages(),
+    install_requires=[
+        'networkx==3.3.0',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: science/Research',
+        'Topic :: Software Development :: libraries',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    extras_require={
+        'test': ['pytest'],
+    }
+)
