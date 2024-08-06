f86075b602356ac3c0ca9ddc35c1513e493e4ad0
ug1y <yinhao@icode.pku.edu.cn>
2024-08-06 11:04:34 +0800

    改为字符串吧，发布0.3.0版本

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
-from .ditto import __version__
 
 setup(
     name='ditto',
-    version=__version__,
+    version='0.3.0',
     author='ug1y',
     author_email='yinhao@icode.pku.edu.cn',
     description='A hybrid blockDAG simulation framework',
